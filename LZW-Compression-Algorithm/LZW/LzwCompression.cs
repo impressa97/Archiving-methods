a73863f66e75a618e57fb164d76d9c309db8bf53
smokecristall <satana2003@gmail.com>
2016-10-11 22:51:37 +0300

    add

---
+++ LZW-Compression-Algorithm/LZW/LzwCompression.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LZW
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public static class LzwCompress
+	{
+		/// <summary>
+		/// Разархивирование файла
+		/// </summary>
+		/// <param name="compressed"></param>
+		/// <returns></returns>
+		 public static string Decompress(List<int> compressed)
+        {
+            // build the dictionary
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add(i, ((char)i).ToString());
+
+            string w = dictionary[compressed[0]];
+            compressed.RemoveAt(0);
+            StringBuilder decompressed = new StringBuilder(w);
+
+            foreach (int k in compressed)
+            {
+                string entry = null;
+                if (dictionary.ContainsKey(k))
+                    entry = dictionary[k];
+                else if (k == dictionary.Count)
+                    entry = w + w[0];
+
+                decompressed.Append(entry);
+
+                // new sequence; add it to the dictionary
+                dictionary.Add(dictionary.Count, w + entry[0]);
+
+                w = entry;
+            }
+
+            return decompressed.ToString();
+        }
+
+		/// <summary>
+		/// Архивирование файла
+		/// </summary>
+		/// <param name="str"></param>
+		/// <returns></returns>
+        public static List<int> Compress(string uncompressed)
+        {
+            // build the dictionary
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add(((char)i).ToString(), i);
+
+            string w = string.Empty;
+            
+            List<int> compressed = new List<int>();
+
+            foreach (char c in uncompressed)
+            {
+                string wc = w + c;
+                
+                Console.WriteLine(wc);
+                if (dictionary.ContainsKey(wc))
+                {
+                    w = wc;
+                }
+                else
+                {
+                    
+                    Console.Write(wc);
+                    compressed.Add(dictionary[w]);
+                    dictionary.Add(wc, dictionary.Count);
+                    w = c.ToString();
+                }
+            }
+
+            // write remaining output if necessary
+            if (!string.IsNullOrEmpty(w))
+                compressed.Add(dictionary[w]);
+
+            return compressed;
+        }
+
+	}
+}
---
+++ LZW-Compression-Algorithm/Main/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+using LZW;
+
+namespace LZW
+{
+    public class Program
+    {
+      
+        public static void Main(string[] args)
+        {
+         
+        	string path = @"I:\SQLQuery\Compress.lzw";
+			string Cpath =  @"I:\SQLQuery\Text.txt";
+			string Dpath = @"I:\SQLQuery\Decompress.txt";
+        
+            string str = string.Empty;
+            string dstr = string.Empty;
+         
+            
+            
+        	using (StreamReader sr = new StreamReader(Cpath,System.Text.Encoding.Default))
+        	{
+                str = sr.ReadToEnd();
+        	}
+			
+            List<int> compressed = LzwCompress.Compress(str); 
+          
+            
+            using (StreamWriter sw =  new StreamWriter(path,false,System.Text.Encoding.Default))
+        	{	
+            	foreach (var element in compressed) 
+            	{
+            		sw.Write(element);
+            	}
+            	
+        	}
+            
+            dstr = LzwCompress.Decompress(compressed);
+            
+            using (StreamWriter sw =  new StreamWriter(Dpath,false,System.Text.Encoding.Default))
+        	{	
+            	sw.Write(dstr);
+        	}
+
+            Console.WriteLine("Сжатие успешно");
+
+            Console.ReadKey();
+            
+        }
+
+
+        
+
+    }
+}

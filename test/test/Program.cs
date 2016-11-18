d031df5c3c5c6df0c6d9d112467007ba40ccbe78
smokecristall <satana2003@gmail.com>
2016-11-18 19:02:08 +0300

    Добавлен нормальный метод

---
+++ test/test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Method;
+
+
+namespace Archiving_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"G:\SQLQuery\Text.txt";  //Отсюда берем
+            string Spath = @"G:\SQLQuery\Compress.txt"; //Сюда кладем
+            string Dpath = @"G:\SQLQuery\DeCompress.txt";// Сюда возвращаем
+            
+            //Arc
+            FileStream InputFileStream = new FileStream(path, FileMode.Open, FileAccess.Read);          
+            FileStream OutputFileStream = new FileStream(Spath, FileMode.Open, FileAccess.Write);
+            Method.Method me = new Method.Method(InputFileStream, OutputFileStream, true);
+            InputFileStream.Close();
+            OutputFileStream.Close();
+
+            //Dearc
+            FileStream InputFileStream1 = new FileStream(Spath, FileMode.Open, FileAccess.Read);
+            FileStream OutputFileStream1 = new FileStream(Dpath, FileMode.Open, FileAccess.Write);
+            Method.Method me1 = new Method.Method(InputFileStream1, OutputFileStream1, false);
+            Console.WriteLine("Gotovo");
+            Console.ReadKey();
+        }
+    }
+}

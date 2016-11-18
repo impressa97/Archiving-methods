f9938df762f3d4af814792022bbf473d2d6a8109
Oleg <chukoleg@gmail.com>
2016-11-18 19:32:31 +0300

    For merge

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,11 +10,18 @@ namespace Archiving_methods
         static void Main(string[] args)
         {
 
+<<<<<<< HEAD:LzwCompression/Archiving-methods/Program.cs
             string path = @"C:\Test\test.txt";  //Отсюда берем
             string Spath = @"C:\Test\Compress.txt"; //Сюда кладем
             string Dpath = @"C:\Test\DeCompress.txt";// Сюда возвращаем
+=======
+            string path = @"G:\SQLQuery\Text.txt";  //Отсюда берем
+            string Spath = @"G:\SQLQuery\Compress.txt"; //Сюда кладем
+            string Dpath = @"G:\SQLQuery\DeCompress.txt";// Сюда возвращаем
+            
+>>>>>>> pr/18:test/test/Program.cs
             //Arc
-            FileStream InputFileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            FileStream InputFileStream = new FileStream(path, FileMode.Open, FileAccess.Read);          
             FileStream OutputFileStream = new FileStream(Spath, FileMode.Open, FileAccess.Write);
             Method.Method me = new Method.Method(InputFileStream, OutputFileStream, true);
             InputFileStream.Close();
@@ -24,6 +31,7 @@ namespace Archiving_methods
             FileStream InputFileStream1 = new FileStream(Spath, FileMode.Open, FileAccess.Read);
             FileStream OutputFileStream1 = new FileStream(Dpath, FileMode.Open, FileAccess.Write);
             Method.Method me1 = new Method.Method(InputFileStream1, OutputFileStream1, false);
+            Console.WriteLine("Gotovo");
             Console.ReadKey();
         }
     }
